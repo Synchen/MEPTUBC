6fcdac45992e431c7cda92a62b07cf47229ac2f3
Patrick <aabraham.patrick@googlemail.com>
2016-10-31 20:15:56 +0100

    Beta Version: - slightly easier input for path and filename

--- meptubc.bat
+++ meptubc.bat
@@ -8,15 +8,20 @@ setlocal enabledelayedexpansion
 
 set path_to_filefolder= Replace
 
+
 :: Replace with path to pscp.exe (Downloadable via http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html)
 :: Example path_to_pscpfolder=C:\putty\
 
 set path_to_pscp= Replace
 
+
 :: Here starts the path to hell
 :: DO NOT CHANGE ANYTHING BENEATH THIS LINE
 
-if exist %path_to_pscp%\pscp.exe (
+if not %path_to_filefolder:~-1% == \	set path_to_filefolder=!path_to_filefolder!\
+if not %path_to_pscp:~-1% == \			set path_to_filefolder=!path_to_pscp!\
+
+if exist !path_to_pscp!pscp.exe (
 echo Enter Account
 echo   Example co1-000
 set /P acc=: 
@@ -24,12 +29,13 @@ echo.
 echo Enter Filename:
 echo   Example PA01 [for PA01.py]
 set /P obj=: 
+if !obj:~-3! == .py set obj=!obj:~,-3!
 echo.
 
-if exist %path_to_filefolder%\!obj!.py (
+if exist !path_to_filefolder!!obj!.py (
 
-%path_to_pscp%\pscp.exe %path_to_filefolder%\!obj!.py !acc!@unixpool.math.tu-berlin.de:./!obj!.py
+!path_to_pscp!pscp.exe !path_to_filefolder!!obj!.py !acc!@unixpool.math.tu-berlin.de:./!obj!.py
 
-) else (echo. & echo !obj!.py does not exist in the folder %path_to_filefolder% ! & echo.)
-) else (echo. & echo Pscp doesn't exist at %path_to_pscp%! & echo.)
+) else (echo. & echo ERROR !obj!.py does not exist in the folder !path_to_filefolder! ! & echo.)
+) else (echo. & echo ERROR Pscp doesn't exist at !path_to_pscp! ! & echo.)
 pause
