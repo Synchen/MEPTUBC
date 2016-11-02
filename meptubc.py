c766b942e8db21d915159de2c88d904de992ccb4
Patrick <aabraham.patrick@googlemail.com>
2016-11-02 17:17:06 +0100

    completed the v1

--- meptubc.py
+++ meptubc.py
@@ -1,38 +1,45 @@
 from os import system, listdir, path, stat
 
-if path.isfile(path.dirname(path.abspath(__file__))+"\\conf.ini"):
-	conf = open(path.dirname(path.abspath(__file__))+"\\conf.ini", "r")
-	conf_read = conf.read().split("\n")
+if path.isfile("%s\\conf.ini" % path.dirname(path.abspath(__file__))):
+    conf = open("%s\\conf.ini" % path.dirname(path.abspath(__file__)), "r")
+    conf_read = conf.read().split("\n")
 
-	if len(conf_read) < 2:
-		input("There was an unexpected ERROR during the setup, please run setup.py again!")
-		system("pause")
-		
-	else:
-		path_to_filefolder	= conf_read[0]
-		path_to_pscp		= conf_read[1]
-	
-		if "pscp.exe" in listdir(path_to_pscp):
+    if len(conf_read) < 2:
+        input(" There was an unexpected ERROR during the setup, please run setup.py again!")
+        system("pause")
+        
+    else:
+        path_to_filefolder	= conf_read[0]
+        path_to_pscp		= conf_read[1]
+    
+        if "pscp.exe" in listdir(path_to_pscp):
 
-			print("Windows scp-script for unixpool@tu-berlin")
-			print("Enter Filename\n  Example PA01.py")
-			obj = input()
-			print("Enter Account\n  Example co1-042")
-			acc = input()
+            print("\n Windows scp-script for unixpool@tu-berlin\n")
+            print(" Enter Filename\n  Example PA01.py\n")
+            obj = input(" >: ")
+            print("\n Enter Account\n  Example co1-042\n")
+            acc = input(" >: ")
+            
+            print("\n        After the file was uploaded,\n        the file can be found in your accounts home-folder.\n")
 
-			if obj in listdir(path_to_filefolder):
-				system("%spscp.exe %s%s %s@unixpool.math.tu-berlin.de:." %(path_to_pscp,path_to_filefolder,obj,acc))
-			
-			else:
-				print(obj + " was not found at " + path_to_filefolder + "!")
-				print("If you want an other filefolder rerun setup.py")
-				system("pause")
-				
-		else:
-			print("pscp.exe was not found in " + path_to_pscp + "!")
-			print("Either move pscp.exe to " + path_to_pscp + " or run setup.py again!")
-			system("pause")
-			
+            if obj in listdir(path_to_filefolder):
+                system("%spscp.exe %s%s %s@unixpool.math.tu-berlin.de:." % (path_to_pscp,path_to_filefolder,obj,acc))
+            
+            else:
+                print(obj + " was not found at %s!" % path_to_filefolder)
+                print(" If you want an other filefolder rerun setup.py")
+                system("pause")
+                
+        else:
+            print(" pscp.exe was not found in %s !" % path_to_pscp)
+            print(" Either move pscp.exe to %s or run setup.py again!" % path_to_pscp)
+            system("pause")
+            
 else:
-	print("Please run setup.py")
-	system("pause")
+    print(" Please run setup.py")
+    system("pause")
+    
+
+#Made by Patrick A.
+#If you have questions, suggestions or found typos send me an email:
+#   meptubc@gmail.com
--- setup.py
+++ setup.py
@@ -2,24 +2,28 @@ from os import path
 
 conf = open(path.dirname(path.abspath(__file__))+"\\conf.ini", "w")
 
-print("Before continuing with the setup!","Please make sure you downloaded Putty scp [pscp.exe]","Either google for Putty/Pscp or visit","  http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html\n", sep="\n")
-print("Please enter path to filefolder containing the files to upload!","  Example: C:\\Users\\Nyancat\\Documents\\Uploadable",sep ="\n")
-ptf = input("_ ")
+print("\n Before continuing with the setup!"," Please make sure you downloaded Putty scp [pscp.exe]"," Either google for Putty/Pscp or visit","   http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html\n", sep="\n")
+print(" Please enter path to filefolder containing the files to upload!","   Example: C:\\Users\\Nyancat\\Documents\\Uploadable",sep ="\n")
+ptf = input(" >: ")
 while not path.isdir(ptf):
-	print("%s is not a correct folder!"% ptf)
-	print("Please input correct folder!")
-	ptf = input(":")
+    print(" %s is not a correct folder!"% ptf)
+    print(" Please input correct folder!")
+    ptf = input(" >: ")
 if not ptf.endswith("\\"):
-	ptf = ptf + "\\"
+    ptf = ptf + "\\"
 
-	
-print("Please enter path to filefolder containing pscp.exe","  Example: C:\\Users\\Nyancat\\Putty\\",sep="\n")
-ptp = input("_ ")
+    
+print(" Please enter path to filefolder containing pscp.exe","   Example: C:\\Users\\Nyancat\\Putty\\",sep="\n")
+ptp = input(" >: ")
 while not path.isdir(ptp):
-	print("%s is not a correct folder!"% ptp)
-	print("Please input correct folder!")
-	ptp = input(":")
+    print(" %s is not a correct folder!"% ptp)
+    print(" Please input correct folder!")
+    ptp = input(" >: ")
 if not ptp.endswith("\\"):
-	ptp = ptp + "\\"
-	
+    ptp = ptp + "\\"
+    
 print(ptf,ptp, sep = "\n", end = "", file = conf)
+
+#Made by Patrick A.
+#If you have qquestions, suggestions or found typos send me an email:
+#   meptubc@gmail.com
